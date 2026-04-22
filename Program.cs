033d01385ff010a322c8282ea27e7b23028120d5
İhsan Eren Erben <147556034+ihsanerben@users.noreply.github.com>
2026-04-22 10:37:37 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ihsanConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World! MY friendo ");
+        }
+    }
+}
